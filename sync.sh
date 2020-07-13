03e3bda25fddf4417ef55660ad9c4a1b7a0d3098
Sebastian Kürten <sebastian@topobyte.de>
2020-07-13 09:31:44 +0200

    Avoid syncing vim swapfiles to the webserver

--- sync.sh
+++ sync.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-rsync --rsync-path="sudo -niu tools.commonsapi rsync" -av --delete public_html/ sebkur@login.toolforge.org:/data/project/commonsapi/public_html
+rsync --rsync-path="sudo -niu tools.commonsapi rsync" -av --delete \
+	--exclude "*.swp" \
+	public_html/ sebkur@login.toolforge.org:/data/project/commonsapi/public_html
